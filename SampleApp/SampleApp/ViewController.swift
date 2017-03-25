6cb246c56d81e0dba95f457551a5468a64f911fe
Savita Juyal <savitajuyal@SaviJ.local>
2017-03-25 22:38:05 +0530

    first commit

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Savita Juyal  on 3/25/17.
+//  Copyright © 2017 Savita Juyal . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
